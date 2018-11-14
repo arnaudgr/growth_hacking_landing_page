00bbc184738126c12b5289b5a36c43ad9afdd03a
Arnaud <arnaud.grandval@gmail.com>
2018-11-14 17:23:54 +0100

    v

--- app/controllers/controller_controller.rb
+++ app/controllers/controller_controller.rb
@@ -1,2 +1,13 @@
 class ControllerController < ApplicationController
+
+	def general
+		
+	end
+
+    def pro
+    end
+
+    def student
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+
+	get "general", to: 'controller#general'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
