4feb534ff3100540b5672eded8ad9a4ccfdd4c3b
Arnaud <arnaud.grandval@gmail.com>
2018-11-15 18:37:21 +0100

    V3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
 	get "/general", to: 'controller#general'
 	post "/register", to: 'contacts#create'
+	get "/student", to: 'controller#student'
+	get "/pro", to: 'controller#pro'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
