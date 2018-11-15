d85ff0f50c6ac2c2ea0afddab4a4ce40e63fe31d
andresromero80 <andresromero80@hotmail.com>
2018-11-15 18:43:52 +0100

    schema modif

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2018_11_15_151256) do
-=======
 ActiveRecord::Schema.define(version: 2018_11_15_171352) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
