4ca06ff22f1257065a125a78246d9867cbc68f97
andresromero80 <andresromero80@hotmail.com>
2018-11-16 16:30:54 +0100

    Add test

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,16 @@ class User < ApplicationRecord
  
   after_create :subscribe_user_to_mailing_list
  
-  private
+  
  
   def subscribe_user_to_mailing_list
     SubscribeUserToMailingListJob.perform_later(self)
   end
 
 
- def send_welcome_email_to_user
-    UserMailer.welcome_email(self).deliver_later
- end
+ # def send_welcome_email_to_user
+ #    UserMailer.welcome_email(self).deliver_later
+ # end
   
 end
 
